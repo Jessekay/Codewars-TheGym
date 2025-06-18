8e22d7f55a9dd9f813e4d59ae8208e848aff8631
JesseKay <kayjes00@gmail.com>
2025-06-18 22:04:45 +0200

    initialized the right and left power sides with 0 value

--- alphabetWar.js
+++ alphabetWar.js
@@ -2,4 +2,8 @@ function alphabetWar(fight) {
     const leftSide = { w: 4, p: 3, b: 2, s:1 };
 
     const rightSide = { m: 4, q: 3, d: 2, z: 1 };
+
+    let leftPower = 0;
+
+    let rightPower = 0;
 }
